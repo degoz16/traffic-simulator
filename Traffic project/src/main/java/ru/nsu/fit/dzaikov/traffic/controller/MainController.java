890563a53ad2fda4752a8053c47bc5386e09e995
villanellex2 <bromkate@mail.ru>
2020-11-05 12:31:24 +0700

    Added OnAction to the buttons on MainView

--- Traffic project/src/main/java/ru/nsu/fit/dzaikov/traffic/controller/MainController.java
+++ Traffic project/src/main/java/ru/nsu/fit/dzaikov/traffic/controller/MainController.java
@@ -10,4 +10,59 @@ public class MainController {
   public void initialize() {
 
   }
+
+  @FXML
+  public void startSimulation(){
+    System.out.println("пуск");
+  }
+
+  @FXML
+  public void pauseSimulation(){
+    System.out.println("пауза");
+  }
+
+  @FXML
+  public void stopSimulation(){
+    System.out.println("стоп");
+  }
+
+  @FXML
+  public void roadButtonClicked(){
+    System.out.println("road");
+  }
+
+  @FXML
+  public void trafficLightButtonClicked(){
+    System.out.println("светофор");
+  }
+
+  @FXML
+  public void buildingButtonClicked(){
+    System.out.println("точки интереса");
+  }
+
+  @FXML
+  public void carButtonClicked(){
+    System.out.println("точки спавна машин");
+  }
+
+  @FXML
+  public void trafficSignButtonClicked(){
+    System.out.println("дорожный знак");
+  }
+
+  @FXML
+  public void showStatistic(){
+    System.out.println("статистика");
+  }
+
+  @FXML
+  public void speedUpSimulation(){
+    System.out.println("быстрее");
+  }
+
+  @FXML
+  public void speedDownSimulation(){
+    System.out.println("медленнее");
+  }
 }
