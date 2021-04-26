1862d0c2160672a02753618c44b857d2037d7b21
katerina <bromkate@mail.ru>
2021-04-26 15:22:07 +0700

    added object painter for global map

--- Traffic_project/src/main/java/ru/nsu/fit/traffic/model/globalmap/RectRegion.java
+++ Traffic_project/src/main/java/ru/nsu/fit/traffic/model/globalmap/RectRegion.java
@@ -30,4 +30,48 @@ public class RectRegion {
   public String getName() {
     return name;
   }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public double getX() {
+    return x;
+  }
+
+  public void setX(double x) {
+    this.x = x;
+  }
+
+  public double getY() {
+    return y;
+  }
+
+  public void setY(double y) {
+    this.y = y;
+  }
+
+  public double getWidth() {
+    return width;
+  }
+
+  public void setWidth(double width) {
+    this.width = width;
+  }
+
+  public double getHeight() {
+    return height;
+  }
+
+  public void setHeight(double height) {
+    this.height = height;
+  }
+
+  public List<RoadConnector> getConnectorList() {
+    return connectorList;
+  }
+
+  public void setConnectorList(List<RoadConnector> connectorList) {
+    this.connectorList = connectorList;
+  }
 }
--- Traffic_project/src/main/java/ru/nsu/fit/traffic/view/GlobalMapObjectPainter.java
+++ Traffic_project/src/main/java/ru/nsu/fit/traffic/view/GlobalMapObjectPainter.java
@@ -1,4 +1,48 @@
 package ru.nsu.fit.traffic.view;
 
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import ru.nsu.fit.traffic.model.globalmap.RectRegion;
+import ru.nsu.fit.traffic.model.globalmap.RoadConnector;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GlobalMapObjectPainter {
+  private final int NODE_SIZE = 10;
+  private final int LANE_SIZE = 10;
+
+  public List<Shape> paintRegion(RectRegion region) {
+    List<Shape> res = new ArrayList<>();
+    Rectangle rect = new Rectangle();
+    rect.setStroke(Color.valueOf("#506070"));
+    rect.setHeight(region.getHeight());
+    rect.setWidth(region.getWidth());
+    rect.setX(region.getX());
+    rect.setY(region.getY());
+    rect.setStrokeWidth(10);
+    rect.setFill(Color.valueOf("transparent"));
+    for (RoadConnector road : region.getConnectorList()) {
+      res.add(paintConnector(road));
+    }
+    return res;
+  }
+
+  public Shape paintConnector(RoadConnector connector) {
+    Circle circle = new Circle();
+    circle.setCenterX(connector.getX() + connector.getRectRegion().getX());
+    circle.setCenterY(connector.getY() + connector.getRectRegion().getY());
+    Image img = new Image(getClass().getResource("Images/connector_on_map.png").toExternalForm());
+    if (img != null) {
+      circle.setFill(new ImagePattern(img));
+    } else {
+      circle.setFill(Paint.valueOf("#101010"));
+    }
+    return circle;
+  }
 }
