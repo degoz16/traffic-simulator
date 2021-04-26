f3c564f98f3231894209505663a1368b3fe30c30
katerina <bromkate@mail.ru>
2021-04-26 12:39:37 +0700

    added global map launcher

---
+++ Traffic_project/src/main/java/ru/nsu/fit/traffic/GlobalMapLauncher.java
@@ -0,0 +1,7 @@
+package ru.nsu.fit.traffic;
+
+public class GlobalMapLauncher {
+    public static void main(String[] args) {
+        GlobalMapApp.main(args);
+    }
+}
--- Traffic_project/src/main/java/ru/nsu/fit/traffic/javafx/controller/edit/GlobalMapController.java
+++ Traffic_project/src/main/java/ru/nsu/fit/traffic/javafx/controller/edit/GlobalMapController.java
@@ -1,4 +1,16 @@
 package ru.nsu.fit.traffic.javafx.controller.edit;
 
+import javafx.fxml.FXML;
+
 public class GlobalMapController {
+    @FXML
+    public void setFragment(){
+
+    }
+
+    @FXML
+    public void setConnector(){
+
+    }
+
 }
