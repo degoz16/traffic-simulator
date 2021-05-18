1ad7dc5e9af56fc7c4882f95eeedb2d9ec978219
katerina <bromkate@mail.ru>
2021-05-19 00:25:03 +0700

    lallalla

--- Traffic_project/src/main/java/ru/nsu/fit/traffic/javafx/controller/FirstMenuController.java
+++ Traffic_project/src/main/java/ru/nsu/fit/traffic/javafx/controller/FirstMenuController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import ru.nsu.fit.traffic.javafx.controller.createMap.CreateMapController;
 
 import java.io.IOException;
 
@@ -24,16 +25,18 @@ public class FirstMenuController {
     @FXML
     public void create_map(){
         FXMLLoader loader = new FXMLLoader(getClass().getResource("../../view/CreateMap.fxml"));
-    System.out.println(getClass());
+        System.out.println(getClass());
         try{
             Parent root = loader.load();
             Scene scene = new Scene(root);
 
             stage.setTitle("Traffic simulator");
             stage.setScene(scene);
+            CreateMapController controller = loader.getController();
+            controller.setStage(stage);
         }
         catch (IOException e){
-            //todo
+            e.printStackTrace();
         }
     }
 
--- Traffic_project/src/main/java/ru/nsu/fit/traffic/javafx/controller/createMap/CreateMapController.java
+++ Traffic_project/src/main/java/ru/nsu/fit/traffic/javafx/controller/createMap/CreateMapController.java
@@ -1,5 +1,54 @@
 package ru.nsu.fit.traffic.javafx.controller.createMap;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import ru.nsu.fit.traffic.javafx.controller.edit.GlobalMapController;
+
+import java.io.IOException;
+
 public class CreateMapController {
 
+  @FXML TextField text_field_wight;
+  @FXML TextField text_field_height;
+
+  Stage stage;
+
+  public void setStage(Stage stage) {
+    this.stage = stage;
+  }
+
+  @FXML
+  public void createMap() {
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("../../../view/GlobalMapView.fxml"));
+    System.out.println(getClass());
+    try {
+      Parent root = loader.load();
+      Scene scene = new Scene(root);
+
+      stage.setTitle("Traffic simulator");
+      stage.setScene(scene);
+      GlobalMapController controller = loader.getController();
+      controller.setStage(stage);
+/* todo: fix
+      controller.setMapParams(
+          Integer.getInteger(text_field_wight.getText()) * 3.3,
+          Integer.getInteger(text_field_height.getText()) * 3.3);
+*/
+      stage.setWidth(850);
+      stage.setHeight(540);
+      stage.show();
+
+      stage.setMinWidth(850);
+      stage.setMinHeight(540);
+
+      stage.setMaxHeight(100000);
+      stage.setMaxWidth(100000);
+    } catch (IOException e) {
+      // todo
+    }
+  }
 }
