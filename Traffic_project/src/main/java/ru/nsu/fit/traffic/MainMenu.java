df21830e74b2ed3840e384c278dc12876ef79297
katerina <bromkate@mail.ru>
2021-06-17 23:03:41 +0700

    reset MainMenu to predebug version

--- Traffic_project/src/main/java/ru/nsu/fit/traffic/MainMenu.java
+++ Traffic_project/src/main/java/ru/nsu/fit/traffic/MainMenu.java
@@ -5,23 +5,29 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import ru.nsu.fit.traffic.javafx.controller.edit.MainController;
 import ru.nsu.fit.traffic.javafx.controller.rooms.LoginController;
 
 public class MainMenu extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(App.class.getResource("view/MainView.fxml"));
+        FXMLLoader loader = new FXMLLoader(App.class.getResource("view/Login.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
 
         stage.setTitle("Traffic simulator");
         stage.setScene(scene);
+        stage.setWidth(310);
+        stage.setMaxWidth(310);
+        stage.setMinWidth(310);
+
+        stage.setHeight(280);
+        stage.setMinHeight(280);
+        stage.setMaxHeight(280);
 
         stage.show();
 
-        MainController controller = loader.getController();
+        LoginController controller = loader.getController();
         controller.setStage(stage);
     }
 
