10364efd4a2791bdcdb02e4bf82ee2e4512e009d
katerina <bromkate@mail.ru>
2021-06-18 22:24:50 +0700

    починила мелькание и красим красным

--- Traffic_project/src/main/java/ru/nsu/fit/traffic/view/GlobalMapEditorViewUpdater.java
+++ Traffic_project/src/main/java/ru/nsu/fit/traffic/view/GlobalMapEditorViewUpdater.java
@@ -1,6 +1,7 @@
 package ru.nsu.fit.traffic.view;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import javafx.application.Platform;
@@ -26,77 +27,97 @@ public class GlobalMapEditorViewUpdater {
   private final ConnectorObserver connectorObserver;
   private final Pane mainPane;
 
-  public GlobalMapEditorViewUpdater(RegionObserver regionObserver, ConnectorObserver connectorObserver, Pane mainPane) {
+  public GlobalMapEditorViewUpdater(
+      RegionObserver regionObserver, ConnectorObserver connectorObserver, Pane mainPane) {
     this.mainPane = mainPane;
     this.regionObserver = regionObserver;
     this.connectorObserver = connectorObserver;
   }
 
-  /**
-   * Метод отрисовки текущего состояния карты
-   */
+  /** Метод отрисовки текущего состояния карты */
   public void updateMapView(GlobalMapEditOpManager editOperationsManager, boolean preview) {
     RegionsMap map = editOperationsManager.getCurrRegMap();
-    mainPane.getChildren().clear();
-    Platform.runLater(() -> {
-      for (int i = 0; i < map.getRegionCount(); i++) {
-        RectRegion region = map.getRegion(i);
-        Rectangle regionShape = painter.paintRegion(region);
-        regionObserver.setRegionMouseHandlers(regionShape, i, region.getWidth(), region.getHeight());
-        mainPane.getChildren().add(regionShape);
-        if (preview) {
-          try {
-            String mapPath = ConnectionConfig.getConnectionConfig()
-              .getConnection()
-              .getMapFromServer(
-                i,
-                ConnectionConfig.getConnectionConfig().getRoomId(),
-                false
-              );
-            TrafficMap trafficMap = EditOperationsManager.loadMap(mapPath);
-            assert trafficMap != null;
-            List<Shape> shapes = painter.paintRegionPreview(region, trafficMap);
-            shapes.forEach(mainPane.getChildren()::add);
-          } catch (Exception e) {
-            e.printStackTrace();
+    List<Shape> mainPaneChild = new ArrayList<>();
+
+    Platform.runLater(
+        () -> {
+          List<Long> blockedMaps = null;
+          if (preview) {
+            blockedMaps =
+                ConnectionConfig.getConnectionConfig()
+                    .getConnection()
+                    .blockedMaps(ConnectionConfig.getConnectionConfig().getRoomId());
+            System.out.println(Arrays.toString(blockedMaps.toArray()));
           }
-        }
-      }
-      map.foreachRegion(region -> {
-        List<Shape> shapes = new ArrayList<>();
-        for (int i = 0; i < region.getConnectorsCount(); i++) {
-          if (region.getConnector(i) == null) {
-            shapes.add(null);
-            continue;
-          }
-          Shape connector = painter.paintConnector(region.getConnector(i), true);
-          shapes.add(connector);
-          mainPane.getChildren().add(connector);
-          connectorObserver.setConnectorObserver(map.getRegions().indexOf(region), i, connector);
-        }
-        if (preview) {
-          try {
-            String mapPath = ConnectionConfig.getConnectionConfig()
-                .getConnection()
-                .getMapFromServer(
-                    editOperationsManager.getCurrRegMap().getRegions().indexOf(region),
-                    ConnectionConfig.getConnectionConfig().getRoomId(),
-                    false
-                );
-            TrafficMap trafficMap = EditOperationsManager.loadMap(mapPath);
-            assert trafficMap != null;
-            trafficMap.forEachNode(node -> {
-              if (node.getConnector() != null) {
-                if (node.getRoadsInNum() == 0 && node.getRoadsOutNum() == 0) {
-                  shapes.get(node.getConnector().getConnectorId()).setFill(new ImagePattern(new Image("ru/nsu/fit/traffic/view/Images/incorrect_connector_on_map.png")));
-                }
+          for (int i = 0; i < map.getRegionCount(); i++) {
+            RectRegion region = map.getRegion(i);
+            Rectangle regionShape = painter.paintRegion(region);
+            regionObserver.setRegionMouseHandlers(
+                regionShape, i, region.getWidth(), region.getHeight());
+            mainPaneChild.add(regionShape);
+            if (preview) {
+              if (blockedMaps != null && blockedMaps.contains((long) i)) {
+                  regionShape.setFill(Color.valueOf("#a08080"));
               }
-            });
-          } catch (Exception e) {
-            e.printStackTrace();
+
+              try {
+                String mapPath =
+                    ConnectionConfig.getConnectionConfig()
+                        .getConnection()
+                        .getMapFromServer(
+                            i, ConnectionConfig.getConnectionConfig().getRoomId(), false);
+                TrafficMap trafficMap = EditOperationsManager.loadMap(mapPath);
+                assert trafficMap != null;
+                List<Shape> shapes = painter.paintRegionPreview(region, trafficMap);
+                mainPaneChild.addAll(shapes);
+              } catch (Exception e) {
+                e.printStackTrace();
+              }
+            }
           }
-        }
-      });
-    });
+          map.foreachRegion(
+              region -> {
+                List<Shape> shapes = new ArrayList<>();
+                for (int i = 0; i < region.getConnectorsCount(); i++) {
+                  if (region.getConnector(i) == null) {
+                    shapes.add(null);
+                    continue;
+                  }
+                  Shape connector = painter.paintConnector(region.getConnector(i), true);
+                  shapes.add(connector);
+                  mainPaneChild.add(connector);
+                  connectorObserver.setConnectorObserver(
+                      map.getRegions().indexOf(region), i, connector);
+                }
+                if (preview) {
+                  try {
+                    String mapPath =
+                        ConnectionConfig.getConnectionConfig()
+                            .getConnection()
+                            .getMapFromServer(
+                                editOperationsManager.getCurrRegMap().getRegions().indexOf(region),
+                                ConnectionConfig.getConnectionConfig().getRoomId(),
+                                false);
+                    TrafficMap trafficMap = EditOperationsManager.loadMap(mapPath);
+                    assert trafficMap != null;
+                    trafficMap.forEachNode(
+                        node -> {
+                          if (node.getConnector() != null) {
+                            if (node.getRoadsInNum() == 0 && node.getRoadsOutNum() == 0) {
+                              shapes
+                                  .get(node.getConnector().getConnectorId())
+                                  .setFill(
+                                      new ImagePattern(
+                                          new Image(
+                                              "ru/nsu/fit/traffic/view/Images/incorrect_connector_on_map.png")));
+                            }
+                          }
+                        });
+                  } catch (Exception e) {
+                    e.printStackTrace();
+                  }
+                }
+              });
+        });
   }
 }
